33734027ae23f0bad1d9926528fe1b61bb233912
leeth5225 <leeth5225@naver.com>
2021-09-29 19:49:56 +0900

    add code for handling contact.html

--- SD_website/app.py
+++ SD_website/app.py
@@ -10,6 +10,10 @@ def index():
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host="0.0.0.0", port=80)
