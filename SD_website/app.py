c7623e6defaee93f23c5a874a766401918a48b61
leeth5225 <leeth5225@naver.com>
2021-09-29 19:44:09 +0900

    add error handler(404) code and a html file

--- SD_website/app.py
+++ SD_website/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host="0.0.0.0", port=80)
